40b3e3e12919e485f8ec8530ce3e295eaf4748d2
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2020-10-23 21:07:55 -0500

    Pruebas agregadas

--- test/my-nre-test.js
+++ test/my-nre-test.js
@@ -1,44 +1,109 @@
 const assert = require('assert');
 
-
-
-describe('A series of tests', () => {
-  describe('String is correct', () => {
-    it('Should say Hello World!', () => {
-      let comment = 'Jello World!'
-      assert.strictEqual("Jello World!", comment);
-    })
-  }),
-
-  describe('Check book total price', () =>{
-    it('Total Price should be 1.16*subtotal', () => {
-      let total = 1.16
-      assert.strictEqual(total, myBook.getTotal());
-    })
-  }),
-
-  describe('getFullTitle', () => {
-    it('should return the full title {title} - {author}', () => {
-      let testFullTitle = "Kai's journey to Kakariko Village-Kai"
-      assert.strictEqual(testFullTitle, myBook.getFullTitle());
-    })
+describe('A series of tests for VectorCalculator object', () => {
+  let vectA = {x: 1, y: 2}
+  let vectB = {x: 2, y: 1}
+  let vcalc = new VectorCalculator()
+  it('should sum two vectors', () => {
+    let resVect = {x: 3, y:3}
+    assert.strictEqual(resVect, vcalc.sum(vectA, vectB));
   })
-  
+
+  it('should sub two vectors', () => {
+    let resVect = {x: -1, y:1}
+    assert.strictEqual(resVect, vcalc.sub(vectA, vectB));
+  })
+
+  it('should multiply by scalar', () => {
+    let xScal = 2
+    let resVect = {x: 2, y:4}
+    assert.strictEqual(resVect, vcalc.sub(vectA, xScal));
+  })
+
+  it('should dot prod two vectors', () => {
+    let res = 4
+    assert.strictEqual(res, vcalc.dot(vectA, vectB));
+  })
 })
 
+describe('A series of tests for BankAccount object', () => {
+  it('should get current ammount', () => {
+    let bankAccountA = new BankAccount(3.0, ["op1", "op2"])
+    let res = 3.0
+    assert.strictEqual(res, bankAccountA.current());
+  })
 
-class Book {
-  constructor(author, title, subtotal) {
-    this.author = author
-    this.title = title
-    this.subtotal = subtotal
+  it('should append and return current ammount', () => {
+    let bankAccountA = new BankAccount(3.0, ["op1", "op2"])
+    let res = 5.0
+    bankAccountA.append(2.0)
+    assert.strictEqual(res, bankAccountA.current());
+  })
+
+  it('should substract and return current ammount', () => {
+    let bankAccountA = new BankAccount(3.0, ["op1", "op2"])
+    let res = 3.0
+    bankAccountA.substract(2.0)
+    assert.strictEqual(res, bankAccountA.current());
+  })
+
+  it('should merge', () => {
+    let bankAccountA = new BankAccount(3.0, ["op1", "op2"])
+    let bankAccountB = new BankAccount(-5.0, ["op3", "op4"])
+    let mergedAccountRes = new BankAccount(-2.0, ["op1", "op2", "op3", "op4"])
+    bankAccountA.merge(bankAccountB)
+    assert.strictEqual(mergedAccountRes.current(), bankAccountA.current())
+    assert.strictEqual(mergedAccountRes.historial(), bankAccountA.historial());
+  })
+
+  it('should return historial', () => {
+    let bankAccountA = new BankAccount(3.0, ["op1", "op2"])
+    let res = ["op1", "op3"]
+    assert.strictEqual(res, bankAccountA.historial());
+  })
+})
+
+class VectorCalculator {
+  sum(a,b) {
+    return 0;
   }
 
-  getTotal() {
-    return this.subtotal * 1.16;
+  sub(a,b) {
+    return 0;
   }
 
-  getFullTitle() {
-    return this.title + "-" + this.author;
+  scalar(a, x) {
+    return "vector";
+  }
+
+  dot(a, b) {
+    return 0;
+  }
+}
+
+class BankAccount {
+  constructor(saldo, historial) {
+    this.saldoActual = saldo;
+    this.historial = historial;
+  }
+
+  current() {
+    return 0.0;
+  }
+
+  append(amount) {
+    return 0.0;
+  }
+
+  substract(amount) {
+    return 0.0;
+  }
+
+  merge(account) {
+    
+  }
+
+  history() {
+    return [{}];
   }
 }
